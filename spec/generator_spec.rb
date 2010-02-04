dc6315a8974450a9d4fdb62f412a858cc39b59c4
Tom Adams <tom@holizz.com>
2010-02-04 10:49:52 +0000

    cwd test.

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -4,4 +4,15 @@ describe WpGenerate::Generator do
   it "should not be possible to instantiate directly" do
     lambda { WpGenerate::Generator.new }.should raise_error NotImplementedError
   end
+
+  it "should correctly determine the cwd" do
+    class BlankGen < WpGenerate::Generator
+      def initialize
+      end
+    end
+
+    BlankGen.new.cwd.should == Dir.pwd
+    ENV['WPGEN_WORK_DIR'] = '/tmp'
+    BlankGen.new.cwd.should == '/tmp'
+  end
 end
